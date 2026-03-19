4f6908bed819360ed9d39ccb76ed5f2134b1f920
shivaprasad <mailtoshivaprasad7@gmail.com>
2026-03-19 19:35:36 +0530

    terraform

---
+++ condition/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "terraform" {
+  ami = "ami-0220d79f3f480ecf5"
+  instance_type = var.environment == "dev" ? "t3.micro" : "t3.medium" # conditional expression to set instance type based on environment variable
+  vpc_security_group_ids = [aws_security_group.allow_all.id] # reference to the security group created above
+ 
+  tags = {
+    Name = "terraform-instance"
+    Terraform = "true"
+  }
+}
+
+resource "aws_security_group" "allow_all" { # this is the security group that allows all traffic
+  name   = "allow-all" # it should be unique within the VPC for user reference
+  
+  egress {
+    from_port        = 0 # from port 0 to port 0 means all ports
+    to_port          = 0
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+    
+  }
+
+  ingress {
+    from_port        = 0 # from port 0 to port 0 means all ports
+    to_port          = 0
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+    
+  }
+
+  tags = {
+    Name = "allow-all" # it should be unique within the VPC for user reference
+    Terraform = "true"
+  }
+}
